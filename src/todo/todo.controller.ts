f4b9ce9d714723366a7f321c2dbe96592df55643
Damilare Bakare <bakaredmlr@gmail.com>
2023-04-22 10:44:59 +0100

    implements get and patch method

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,6 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch } from '@nestjs/common';
+import { TodoDto } from './todo.dto';
+import { TodoService } from './todo.service';
 
 @Controller('todo')
 export class TodoController {
-    
+    constructor(private todoService: TodoService) {}
+
+    @Get()
+    getTodos() {
+        return this.todoService.getTodos();
+    }
+
+    @Get(':id')
+    getTodoById(@Param('id')id: number) {
+        return this.todoService.getTodoById(id);
+    }
+
+    @Patch(':id')
+    updateTodoById(@Param('id')id: number, @Body()todo: TodoDto) {
+        return this.todoService.updateTodoById(id, todo);
+    }
 }
